adb6c8366fb758f1c200f2659201991998304ce9
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-06-07 10:35:17 +0200

    Test API spec endpoint

--- assert/apps.go
+++ assert/apps.go
@@ -2,10 +2,19 @@ package assert
 
 import (
 	"testing"
+
+	"github.com/eliona-smart-building-assistant/app-integration-tests/app"
 )
 
 func AppWorks(t *testing.T) bool {
+	metadata, err := app.GetMetadata()
+	if err != nil {
+		t.Fatalf("getting metadata: %v", err)
+	}
+
 	IconFileIsValid(t)
-	VersionEndpointExists(t)
+	VersionEndpointExists(t, metadata)
+	APISpecEndpointExists(t, metadata)
+
 	return t.Failed()
 }
--- assert/endpoints.go
+++ assert/endpoints.go
@@ -2,8 +2,11 @@ package assert
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
+
+	"github.com/eliona-smart-building-assistant/app-integration-tests/app"
 )
 
 type VersionResponse struct {
@@ -11,8 +14,9 @@ type VersionResponse struct {
 	Timestamp string `json:"timestamp"`
 }
 
-func VersionEndpointExists(t *testing.T) bool {
-	resp, err := http.Get("http://localhost:3039/v1/version")
+func VersionEndpointExists(t *testing.T, metadata app.Metadata) bool {
+	url := fmt.Sprintf("http://localhost:3039/%s/version", metadata.ApiUrl)
+	resp, err := http.Get(url)
 	if err != nil {
 		t.Fatalf("Failed to send request to /version: %s", err)
 	}
@@ -34,4 +38,23 @@ func VersionEndpointExists(t *testing.T) bool {
 		t.Error("Timestamp field is empty")
 	}
 	return true
+}
+
+func APISpecEndpointExists(t *testing.T, metadata app.Metadata) {
+	url := "http://localhost:3039/" + metadata.ApiUrl + metadata.ApiSpecificationPath
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatalf("Failed to send request to %s: %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status OK but got %d", resp.StatusCode)
+	}
+
+	var apiSpecResponse interface{}
+	err = json.NewDecoder(resp.Body).Decode(&apiSpecResponse)
+	if err != nil {
+		t.Fatalf("Failed to decode response body: %s", err)
+	}
 }
